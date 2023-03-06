5278c62844905ffdef4d58ddf63704a554733a69
mfahadarif85 <mfahadarif@gmail.com>
2023-03-06 23:29:00 +0500

    app init

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    </div>
+  );
+}
